97ee8f8e252deee1eecb89c50af024b8f6cf9f46
Qayyum3008 <140565850+Qayyum3008@users.noreply.github.com>
2025-11-05 16:28:18 +0100

    Add files via upload

---
+++ src/features/order/SearchOrder.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function SearchOrder() {
+  const navigate = useNavigate();
+  const [query, setQuery] = useState('');
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!query) return;
+    navigate(`/order/${query}`);
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Seach order #"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        className="duration:300 w-24 rounded-full px-4 py-2 text-sm transition-all placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
+      />
+    </form>
+  );
+}
+
+export default SearchOrder;
